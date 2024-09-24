f63a2bf2da485d6e9352918af757ae72a9aac79e
1987_ Mohammad Habib Ullah <mohammadhabibullah937@gmail.com>
2024-09-24 20:27:26 +0600

    first commit

---
+++ js/function.js
@@ -0,0 +1,11 @@
+
+function getInputFieldValueById(value){
+    let result=document.getElementById(value).value
+    let result_int=parseFloat(result)
+    return result_int
+}
+function getTextFieldValueById(value){
+    let result=document.getElementById(value).innerText
+    let result_int=parseFloat(result)
+    return result_int
+}
